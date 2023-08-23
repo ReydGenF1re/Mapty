5ec7d614b72ece4bc32d92a63a52726db1bcb1f1
Sergey <93822121+ReydGenF1re@users.noreply.github.com>
2023-08-23 18:59:59 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -111,7 +111,7 @@ class App {
 
     this.#map = L.map("map").setView(coords, this.#mapZoomLevel);
 
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png", {
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
